f1bc742f24e1a8bcaf62799e363f4e8b680b54e7
Anna Kapranova <annakapranova77@gmail.com>
2019-10-08 22:26:06 +0300

    fix api

--- impl/src/main/java/com/shop/controller/ProductController.java
+++ impl/src/main/java/com/shop/controller/ProductController.java
@@ -5,11 +5,13 @@ import com.shop.api.swagger.models.ProductDTO;
 import com.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("${api.prefix}")
 public class ProductController implements ProductManagementApi {
     private ProductService service;
 
