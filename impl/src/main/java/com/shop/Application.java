daf6e5c75fb9043c3bbb4c979e75f311b6fc7dbb
Anna Kapranova <annakapranova77@gmail.com>
2019-12-17 20:54:24 +0300

    add dockerfile and some fixes

--- impl/src/main/java/com/shop/Application.java
+++ impl/src/main/java/com/shop/Application.java
@@ -2,10 +2,12 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
